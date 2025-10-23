bc4666ea77e8f747788072affbe3fb122ded773e
Ivan Sukhankin <sukhankin.is@phystech.edu>
2025-10-23 04:52:22 +0300

    NVIM: add quick-scope plugin

---
+++ nvim/.config/nvim/lua/plugins/hints/quick-scope.lua
@@ -0,0 +1,16 @@
+return {
+  "unblevable/quick-scope",
+}
+
+-- NOTE: maybe later
+-- return {
+--   'jinh0/eyeliner.nvim',
+--   opts = {
+--       -- show highlights only after keypress
+--       -- highlight_on_key = true,
+--       highlight_on_key = false,
+--
+--       -- dim all other characters if set to true (recommended!)
+--       dim = true,             
+--   },
+-- }
