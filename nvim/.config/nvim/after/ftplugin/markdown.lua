dbbdca8b0ead5c83c7698fa98ba9e29e74f9245e
Ivan Sukhankin <sukhankin.is@phystech.edu>
2025-10-30 06:05:12 +0300

    NVIM: Set ftplugin for markdown (lists)

--- nvim/.config/nvim/after/ftplugin/markdown.lua
+++ nvim/.config/nvim/after/ftplugin/markdown.lua
@@ -1,1 +1,14 @@
-vim.opt_local.formatoptions:append("rno")
+local opt_local = vim.opt_local
+
+opt_local.formatoptions:append("r") -- '<CR>' in insert mode
+opt_local.formatoptions:append("o") -- '<CR>' in insert mode
+
+opt_local.comments = {
+  -- "fb:*,fb:-,fb:+,n:>", -- default
+  "b:- [ ]", -- tasks
+  "b:- [x]",
+  "b:*", -- unordered list
+  "b:-",
+  "b:+",
+  "On:>", -- quote
+}
