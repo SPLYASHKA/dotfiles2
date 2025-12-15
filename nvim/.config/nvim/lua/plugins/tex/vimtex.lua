411affda3b26a0b7a29ccf49a524a359ff13b82b
Ivan Sukhankin <sukhankin.is@phystech.edu>
2025-12-15 16:10:34 +0300

    NVIM: Fix vimtex config

--- nvim/.config/nvim/lua/plugins/tex/vimtex.lua
+++ nvim/.config/nvim/lua/plugins/tex/vimtex.lua
@@ -1,6 +1,8 @@
+-- TODO: можно сделать на маке чтобы пдфка открывалась и обновялалсь ну и линки
 return {
   "lervag/vimtex",
-  lazy = true,     -- we don't want to lazy load VimTeX
+  enabled = true,
+  lazy = true, -- we don't want to lazy load VimTeX UPD: why?
   ft = "tex",
   -- tag = "v2.15", -- uncomment to pin to a specific release
   init = function()
@@ -9,12 +11,13 @@ return {
     local opt = vim.opt
 
     g.vimtex_view_method = "zathura"
-    g.tex_flavor='latex'
+    g.tex_flavor = 'latex'
     g.vimtex_quickfix_mode = 0
     -- opt.conceallevel = 1
     g.tex_conceal = 'adbmg'
   end,
   config = function()
+    local opt = vim.opt
     opt.conceallevel = 1
   end
 }
