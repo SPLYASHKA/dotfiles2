8b0271b0dbde6bc72e9c2c5c2a7c1c9dda9e194c
Ivan Sukhankin <sukhankin.is@phystech.edu>
2025-12-15 14:41:53 +0300

    NVIM: Remove plugins I no longer use

--- nvim/.config/nvim/lua/plugins/hints/hardtime.lua
+++
@@ -1,7 +0,0 @@
--- TODO: как и precognition может неистово раздражать, посмотрим какое время они проживут у меня в конфиге
-return {
-   "m4xshen/hardtime.nvim",
-   lazy = false,
-   dependencies = { "MunifTanjim/nui.nvim" },
-   opts = {},
-}
