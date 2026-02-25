2d5c538704adb9486f5c19d8862d62dce3d2ecb3
Ivan Sukhankin <sukhankin.is@phystech.edu>
2026-02-25 16:58:44 +0300

    NVIM: Add texpresso plugin

---
+++ nvim/.config/nvim/lua/plugins/tex/texpresso.lua
@@ -0,0 +1,6 @@
+-- https://github.com/let-def/texpresso.vim
+-- NOTE: need to install texpresso and you should have texpresso binary in your PATH.
+-- https://github.com/let-def/texpresso
+return {
+  "let-def/texpresso.vim",
+}
