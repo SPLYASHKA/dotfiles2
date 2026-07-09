42efac0e0889a2f69a05248a08ea8e230d5d5874
Ivan Sukhankin <sukhankin.is@phystech.edu>
2026-07-09 19:17:33 +0300

    NVIM: register typst plugin group import in init.lua

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -8,6 +8,7 @@ return {
   { import = "plugins.git" },
   { import = "plugins.snippets" },
   { import = "plugins.tex" },
+  { import = "plugins.typst" },
   { import = "plugins.plugins_dev" },
   { import = "plugins.image" },
   { import = "plugins.cpp" },
